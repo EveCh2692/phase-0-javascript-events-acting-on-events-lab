2487cc0347a3810e507184d2db2473b7b4e37b52
Evelyn <evelyn.chavez92@yahoo.com>
2021-08-05 14:05:45 -0500

    Done.

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ document.addEventListener("keydown", function(e) {
 });
 
 function moveDodgerRight() {
-    const rightNumbers = dodger.style.left.replace("px", "");
-    const right = parseInt(rightNumbers, 10);
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
 
-    if (right < 0) {
-        dodger.style.left = `${right + 1}px`;
+    if (left < 180) {
+        dodger.style.left = `${left + 1}px`;
     }
 }
 
